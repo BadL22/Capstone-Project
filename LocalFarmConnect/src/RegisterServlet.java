1c1d90344f5f266fa86c9a00972c4084cb89b6d2
BadL22 <140843288+BadL22@users.noreply.github.com>
2025-04-26 15:16:19 -0400

    Add files via upload

---
+++ LocalFarmConnect/src/RegisterServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import dao.UserDAO;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Get form values
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+        String location = request.getParameter("location");
+
+        // Create new User object
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+        user.setLocation(location);
+
+        // Insert into DB
+        UserDAO userDAO = new UserDAO();
+        boolean success = userDAO.registerUser(user);
+
+        if (success) {
+            response.sendRedirect("register-success.jsp"); // Redirect to success page (you can create this JSP later)
+        } else {
+            response.sendRedirect("register-failure.jsp"); // Redirect to failure page (also create later)
+        }
+    }
+}
