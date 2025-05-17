d3087cf6846a668d194345b86644202eff5119b2
BadL22 <140843288+BadL22@users.noreply.github.com>
2025-05-17 10:56:07 -0400

    Refactor LoginServlet.java

--- LocalFarmConnect/src/servlet/LoginServlet.java
+++ LocalFarmConnect/src/servlet/LoginServlet.java
@@ -5,31 +5,29 @@ import model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    private final UserDAO userDAO = UserDAO.getInstance(); // Singleton Pattern
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        UserDAO userDAO = new UserDAO();
         User user = userDAO.loginUser(email, password);
 
         if (user != null) {
             HttpSession session = request.getSession();
             session.setAttribute("currentUser", user);
-            response.sendRedirect("login-success.jsp"); // Create later
+            response.sendRedirect("views/dashboard.jsp");
         } else {
-            response.sendRedirect("login-failure.jsp"); // Create later
+            response.sendRedirect("views/login-failure.jsp");
         }
     }
 }
