dd875c60f57b79f983a9061321bc236cacc7999e
BadL22 <140843288+BadL22@users.noreply.github.com>
2025-05-17 10:56:37 -0400

    Refactored User.java

--- LocalFarmConnect/src/model/User.java
+++ LocalFarmConnect/src/model/User.java
@@ -8,64 +8,22 @@ public class User {
     private String role;
     private String location;
 
-    public User() {
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public User(int id, String name, String email, String password, String role, String location) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.location = location;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getName() {
-        return name;
-    }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
 }
+
